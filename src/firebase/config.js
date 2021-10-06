ab88e7d64573b550be580c95e6bae53b724b54e1
edward frempong <eddysoftwarengineer@gmail.com>
2021-10-06 18:02:37 +0000

    added a new config file from firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyAFbqFPAZjCUBXbOTvGq4rxV8jV79dFTvs",
-    authDomain: "contact-firebase-7e373.firebaseapp.com",
-    projectId: "contact-firebase-7e373",
-    storageBucket: "contact-firebase-7e373.appspot.com",
-    messagingSenderId: "1065977572057",
-    appId: "1:1065977572057:web:cbd3a3499c1a97899dacc8",
-    measurementId: "G-2YVDX2TVRJ"
+  const firebaseConfig = {
+    apiKey: "AIzaSyDBBOR58WBoWq22cC7_j-uc4nCCzqa-W9Q",
+    authDomain: "contact-app-firebase-d7aef.firebaseapp.com",
+    projectId: "contact-app-firebase-d7aef",
+    storageBucket: "contact-app-firebase-d7aef.appspot.com",
+    messagingSenderId: "34033793821",
+    appId: "1:34033793821:web:657ae7682a07a165523ff4",
+    measurementId: "G-XN1F9T9VZX"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
